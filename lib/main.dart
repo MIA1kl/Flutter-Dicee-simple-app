ef82fb4abfddbeda7ca1ca02cb685ead95375204
MIA1kl <mia01kln@gmail.com>
2022-02-14 01:21:33 +0600

    Added random numbers and functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,28 +16,42 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
           Expanded(
-            child: FlatButton(
+            child: TextButton(
                 onPressed: (){
-                  print('Left button was clecked');
+                  setState(() {
+                    leftDice = Random().nextInt(6) +1;
+                    rightDice = Random().nextInt(6) +1;
+                  });
                 },
-                child: Image.asset('images/dice1.png')),
+                child: Image.asset('images/dice$leftDice.png')),
           ),
           Expanded(
-            child: FlatButton(
+            child: TextButton(
                 onPressed: (){
-                  print('Right buttom was clicked');
+                  setState(() {
+                    leftDice = Random().nextInt(6) +1;
+                    rightDice = Random().nextInt(6) +1;
+                  });
                 },
-                child: Image.asset('images/dice2.png')),
+                child: Image.asset('images/dice$rightDice.png')),
           ),
         ],
       ),
     );
   }
 }
+
